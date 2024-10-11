fe264edc3278c16e6fbfbe939984177e96dc612c
Diogo Valadares Reis dos Santos <58271577+Diogo-Valadares@users.noreply.github.com>
2024-10-11 17:17:54 -0300

    Changed CC selection
    The CC selection now follows the Katevenis order in the book about RISC II

--- src/Assembler/Condition.cs
+++ src/Assembler/Condition.cs
@@ -2,40 +2,42 @@
 #pragma warning disable CA1069
 public enum Condition
 {
-    ALWAYS = 0x0,
-    NEVER = 0x1,
-    NOT_EQUAL = 0x2,
-    EQUAL = 0x3,
-    GREATER = 0x4,
-    GREATER_EQUAL = 0x5,
-    LESS = 0x6,
-    LESS_EQUAL = 0x7,
-    POSITIVE = 0x8,
-    NEGATIVE = 0x9,
-    HIGHER = 0xA,
-    LOWER_SAME = 0xB,
-    OVERFLOW_CLEAR = 0xC,
-    OVERFLOW_SET = 0xD,
-    CARRY_CLEAR = 0xE,
-    CARRY_SET = 0xF,
+    NEVER = 0x0,
+    GREATER = 0x1,
+    LESS_EQUAL = 0x2,
+    GREATER_EQUAL = 0x3,
+    LESS = 0x4,
+    HIGHER = 0x5,
+    LOWER_SAME = 0x6,
+    CARRY_CLEAR = 0x7,
+    LOWER = 0x7,
+    CARRY_SET = 0x8,
+    HIGHER_OR_SAME = 0x8,
+    POSITIVE = 0x9,
+    NEGATIVE = 0xA,
+    NOT_EQUAL = 0xB,
+    EQUAL = 0xC,
+    OVERFLOW_CLEAR = 0xD,
+    OVERFLOW_SET = 0xE,
+    ALWAYS = 0xF,
     //Short Terms according to katevenis RISC II book
-    ALW = 0x0,  //always
-    NEV = 0x1,  //never // does not exist on katevenis definition
-    NE = 0x2,   //not equak
-    EQ = 0x3,   //equals
+    NEV = 0x0,  //never // does not exist on katevenis definition
     GT = 0x4,   //Greater than
+    LE = 0x7,   //less or equal
     GE = 0x5,   //Greater or equal
     LT = 0x6,   //less than
-    LE = 0x7,   //less or equal
-    PL = 0x8,   //plus
-    MI = 0x9,   //minus
     HI = 0xA,   //higher than
     LOS = 0xB,  //lower or same
-    NV = 0xC,   //no overflow
-    V = 0xD,    //overflow
     NC = 0xE,   //no carry
     LO = 0xE,   //lower than
     C = 0xF,    //carry
-    his = 0xF   //higher or same
+    HIS = 0xF,   //higher or same
+    PL = 0x8,   //plus
+    MI = 0x9,   //minus
+    NE = 0x2,   //not equal
+    EQ = 0x3,   //equals
+    NV = 0xC,   //no overflow
+    V = 0xD,    //overflow
+    ALW = 0x0,  //always
 }
 #pragma warning restore CA1069
