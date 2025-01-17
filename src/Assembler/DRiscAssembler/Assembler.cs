c8642115d3cbf17de698fac6c070927f94ea1a72
Diogo Valadares Reis dos Santos <58271577+Diogo-Valadares@users.noreply.github.com>
2025-01-17 17:24:00 -0300

    DRISC Assembler
    Added a new assembler algorithm that compiles code in the RISC-V format, with the same instructions and register name parsing. it also prints the assembly steps in a simpler and color coded way to make debbugging easier.
    
    The old assembler is still here and to activate it you need to activate in the config file changing the version from DRisc to anything else

---
+++ src/Assembler/DRiscAssembler/Assembler.cs
@@ -0,0 +1,186 @@
+﻿namespace DRiscAssembler;
+public class Assembler
+{
+    private static readonly string[] separator = ["\r\n", "\r", "\n"];
+
+    public static string Assemble(string input, int bitWidth, int addressWidth)
+    {
+        var lines = input.Replace("\t", "").Split(separator, StringSplitOptions.None);
+        var memory = new uint[2 << addressWidth];
+        var linesList = lines.ToList();
+        RemoveCommentsAndBlankLines(linesList);
+        Dictionary<string, uint> labels = ExtractLabels(linesList);
+        Dictionary<string, uint> words = ExtractVariables(linesList);
+        lines = [.. linesList];
+
+        for (uint currentLine = 0, address = 0; currentLine < lines.Length; currentLine++, address++)
+        {
+            var parts = lines[currentLine].Split(' ');
+
+            switch (parts[0].ToUpper())
+            {
+                case ".WORD":
+                    memory[address] = Instruction.ToInteger(parts[2], 0xffffffff);
+                    Console.Write($"\n[{currentLine}]:\t");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(Instruction.ToBinary(memory[address]));
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.Write($" {parts[0]}");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write($" {parts[1]}");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write($" {parts[2]}");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    continue;
+
+            }
+
+            if (!Instruction.instructions.TryGetValue(parts[0].ToUpper(), out var translator))
+            {
+                throw new Exception($"Unknown instruction \"{parts[0]}\" at line {currentLine}.");
+            }
+            Console.Write($"\n[{currentLine}]:\t");
+
+            memory[address] = translator(parts[1..]);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write($"  {parts[0]}");
+            for (int i = 1; i < parts.Length; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                if (!int.TryParse(parts[i], out _) && Enum.TryParse(typeof(Register), parts[i], true, out _))
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                }
+
+                Console.Write($" {parts[i]}");
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        Console.Write("\n\n");
+        var output = "v3.0 hex words addressed";
+        uint mask = uint.MaxValue >> (32 - bitWidth);
+        for (int i = 0, address = 0; i < memory.Length; i++, address += 32 / bitWidth)
+        {
+            if (i % (bitWidth / 2) == 0) output += $"\n{address:x8}:";
+
+            for (int j = 0; j <= 32 - bitWidth; j += bitWidth)
+            {
+                var portion = (memory[i] >> j) & mask;
+                output += $" {portion.ToString($"x{bitWidth / 4}")}";
+            }
+        }
+        return output;
+    }
+    public static void PrintInstructions()
+    {
+        foreach (var instruction in Instruction.instructions)
+        {
+            Console.Write(instruction.Key + " ");
+        }
+    }
+
+    private static void RemoveCommentsAndBlankLines(List<string> lines)
+    {
+        for (int currentLine = lines.Count - 1; currentLine >= 0; currentLine--)
+        {
+            var commentIndex = lines[currentLine].IndexOf("//");
+            if (commentIndex >= 0) lines[currentLine] = lines[currentLine][0..(commentIndex)];
+            if (lines[currentLine] == "")
+            {
+                lines.RemoveAt(currentLine);
+            }
+        }
+    }
+    private static Dictionary<string, uint> ExtractLabels(List<string> lines)
+    {
+        Dictionary<string, uint> labels = [];
+        for (int currentLine = 0; currentLine < lines.Count; currentLine++)
+        {
+            if (!lines[currentLine].StartsWith(':')) continue;
+            labels.Add(lines[currentLine], (uint)currentLine << 2);
+            Console.WriteLine($"[{currentLine}]Added label \"{lines[currentLine]}\" with value {(uint)currentLine << 2}");
+            lines.RemoveAt(currentLine);
+            currentLine--;
+        }
+
+        for (int currentLine = 0; currentLine < lines.Count; currentLine++)
+        {
+            var parts = lines[currentLine].Split(' ');
+            for (int i = 0; i < parts.Length; i++)
+            {
+                if (!parts[i].StartsWith(':')) continue;
+                if (!labels.TryGetValue(parts[i], out var value))
+                {
+                    throw new Exception($"[{currentLine}]Unknown label \"{parts[i]}\" at line {currentLine}.");
+                }
+                var lineBefore = lines[currentLine];
+                lines[currentLine] = lines[currentLine].Replace(parts[i], value.ToString());
+                #region DebugPrint
+                Console.Write($"[{currentLine}]Translated label at line from ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write($"\"{lineBefore}\"");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(" to ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"\"{lines[currentLine]}\"");
+                Console.ForegroundColor = ConsoleColor.White;
+                #endregion
+            }
+        }
+        return labels;
+    }
+    private static Dictionary<string, uint> ExtractVariables(List<string> lines)
+    {
+        Dictionary<string, uint> words = [];
+        for (int currentLine = 0; currentLine < lines.Count; currentLine++)
+        {
+            var parts = lines[currentLine].Split(' ');
+            switch (parts[0])
+            {
+                case ".word":
+                    words.Add(parts[1], (uint)currentLine << 2);
+                    break;
+                case ".short":
+                case ".half":
+                case ".byte":
+                    throw new NotImplementedException();
+            }
+        }
+
+        if (words.Count > 0)
+        {
+            Console.WriteLine("\nVariables:");
+            foreach (var word in words)
+            {
+                Console.WriteLine($"{word.Key} = {word.Value}");
+            }
+        }
+
+        for (int currentLine = 0; currentLine < lines.Count; currentLine++)
+        {
+            var parts = lines[currentLine].Split(' ');
+            for (int i = 1; i < parts.Length; i++)
+            {
+                if (!parts[i].StartsWith('.')) continue;
+                if (!words.TryGetValue(parts[i][1..], out var value))
+                {
+                    throw new Exception($"[{currentLine}]Unknown variable \"{parts[i]}\" at line {currentLine}.");
+                }
+                var lineBefore = lines[currentLine];
+                lines[currentLine] = lines[currentLine].Replace(parts[i], value.ToString());
+                #region DebugPrint
+                Console.Write($"[{currentLine}]Translated variable at line from ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write($"\"{lineBefore}\"");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(" to ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"\"{lines[currentLine]}\"");
+                Console.ForegroundColor = ConsoleColor.White;
+                #endregion
+            }
+        }
+
+        return words;
+    }
+}
---
+++ src/Assembler/DRiscAssembler/Enums.cs
@@ -0,0 +1,141 @@
+﻿namespace DRiscAssembler;
+public enum Instructions
+{
+    load = 0x03,
+    load_fp = 0x07,
+
+    opimm = 0x13,
+    auipc = 0x17,
+
+    store = 0x23,
+    store_fp = 0x2f,
+
+    op = 0x33,
+    lui = 0x37,
+
+    op_fp = 0x53,
+
+    branch = 0x63,
+    jalr = 0x67,
+    jal = 0x6f,
+
+    system = 0x73,
+
+}
+public enum DataSize
+{
+    @byte = 0x0,
+    half = 0x1,
+    word = 0x2,
+    @float = 0x3,
+    ubyte = 0x4,
+    uhalf = 0x5,
+}
+public enum Operation
+{
+    add = 0x00,
+    addi = add,
+    sub = 0x100,
+    sll = 0x01,
+    slli = sll,
+    slt = 0x02,
+    slti = slt,
+    sltu = 0x03,
+    sltiu = sltu,
+    xor = 0x04,
+    xori = xor,
+    srl = 0x05,
+    srli = srl,
+    sra = 0x105,
+    srai = sra,
+    or = 0x06,
+    ori = or,
+    and = 0x07,
+    andi = and,
+
+    mul = 0x10,
+    mulh = 0x11,
+    mulhsu = 0x12,
+    mulhu = 0x13,
+    div = 0x14,
+    divu = 0x15,
+    rem = 0x16,
+    remu = 0x17
+}
+public enum Branch
+{
+    beq = 0x00,
+    bne = 0x01,
+    blt = 0x04,
+    bge = 0x05,
+    bltu = 0x06,
+    bgeu = 0x07
+}
+public enum Register
+{
+    x0 = 0x00,
+    zero = x0,
+    x1 = 0x01,
+    ra = x1,
+    x2 = 0x02,
+    sp = x2,
+    x3 = 0x03,
+    gp = x3,
+    x4 = 0x04,
+    tp = x4,
+    x5 = 0x05,
+    t0 = x5,
+    x6 = 0x06,
+    t1 = x6,
+    x7 = 0x07,
+    t2 = x7,
+    x8 = 0x08,
+    s0 = x8,
+    fp = x8,  // Frame pointer alias
+    x9 = 0x09,
+    s1 = x9,
+    x10 = 0x0a,
+    a0 = x10,
+    x11 = 0x0b,
+    a1 = x11,
+    x12 = 0x0c,
+    a2 = x12,
+    x13 = 0x0d,
+    a3 = x13,
+    x14 = 0x0e,
+    a4 = x14,
+    x15 = 0x0f,
+    a5 = x15,
+    x16 = 0x10,
+    a6 = x16,
+    x17 = 0x11,
+    a7 = x17,
+    x18 = 0x12,
+    s2 = x18,
+    x19 = 0x13,
+    s3 = x19,
+    x20 = 0x14,
+    s4 = x20,
+    x21 = 0x15,
+    s5 = x21,
+    x22 = 0x16,
+    s6 = x22,
+    x23 = 0x17,
+    s7 = x23,
+    x24 = 0x18,
+    s8 = x24,
+    x25 = 0x19,
+    s9 = x25,
+    x26 = 0x1a,
+    s10 = x26,
+    x27 = 0x1b,
+    s11 = x27,
+    x28 = 0x1c,
+    t3 = x28,
+    x29 = 0x1d,
+    t4 = x29,
+    x30 = 0x1e,
+    t5 = x30,
+    x31 = 0x1f,
+    t6 = x31
+}
